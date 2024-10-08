4da831f603d44960dfba32db814c4e30fcd385e3
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-10-08 18:17:57 +0200

    add nix config

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Bot to manage groceries";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        defaultPackage = pkgs.buildGo123Module {
+          pname = "listedecourse";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = "sha256-0GfassNv0886Z1D6PB9wdMkmRXfaaXeGJKDDgSJ5KJM=";
+        };
+      }
+    );
+}
