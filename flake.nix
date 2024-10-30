8d9d0289a59ecb9e4d44a4352ea42206d8f0a963
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-10-30 14:16:32 +0100

    add checks

--- flake.nix
+++ flake.nix
@@ -4,12 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -22,21 +25,10 @@
           vendorHash = "sha256-0GfassNv0886Z1D6PB9wdMkmRXfaaXeGJKDDgSJ5KJM=";
         };
       }
-    )
-    // (
-      let
-        pkgsCrossAarch64 = import nixpkgs {
-          system = "x86_64-linux";
-          crossSystem.config = "aarch64-unknown-linux-gnu";
-        };
-      in
-      {
-        packages.cross.listedecourse = pkgsCrossAarch64.buildGo123Module {
-          pname = "listedecourse";
-          version = "0.1.0";
-          src = ./.;
-          vendorHash = "sha256-0GfassNv0886Z1D6PB9wdMkmRXfaaXeGJKDDgSJ5KJM=";
-        };
-      }
-    );
+    ))
+    // {
+      checks = {
+        listedecourse-aarch64-linux = self.defaultPackage.aarch64-linux;
+      };
+    };
 }
